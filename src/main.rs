c78218e39d27efa23bf0dcdd8e23b0a1e453194c
AppleThomas <thomasnguyen3212@gmail.com>
2023-06-21 18:13:47 -0400

    started to add logging

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@ use zero2prod::startup::run;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use sqlx::PgPool;
+use env_logger::Env;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     // panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
--- src/startup.rs
+++ src/startup.rs
@@ -1,17 +1,19 @@
-use actix_web::{web, App, HttpServer};
-use std::net::TcpListener;
-use actix_web::dev::Server;
 use crate::routes::{
     subscribe, health_check
 };
+use std::net::TcpListener;
+use actix_web::dev::Server;
+use actix_web::web::Data;
+use actix_web::{web, App, HttpServer};
+use actix_web::middleware::Logger;
 use sqlx::PgPool;
-
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // wrap the connection in a smart pointer
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || { 
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             // get a pointer copy and attach it to the application state
