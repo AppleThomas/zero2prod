94fe40aa772a88c8fb68bb364b67cb9d474a2d65
AppleThomas <thomasnguyen3212@gmail.com>
2023-07-18 01:46:31 -0400

    created database in digital ocean and did additional script changes

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use secrecy::ExposeSecret;
 use zero2prod::startup::run;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
-use sqlx::PgPool;
+use sqlx::{PgPool, postgres::PgPoolOptions};
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 
@@ -15,8 +15,11 @@ async fn main() -> std::io::Result<()> {
 
     // panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
-    .expect("Failed to connect to Postgres")
+    
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.database.with_db());
+
 ;    let address = format!("{}:{}",configuration.application.host, configuration.application.port);
 
     let listener = TcpListener::bind(address)
